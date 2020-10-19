cfcb6dfbed548230943042bbbf970b1c12a46cb1
MANALCH <manalch@oocl.com>
2020-10-19 15:49:30 +0800

    [Word-Frequency][joseph] refactor: change variable name s to line

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -33,8 +33,8 @@ public class WordFrequencyGame {
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (WordInfo wordInfo : wordInfoList) {
-                    String s = wordInfo.getValue() + " " + wordInfo.getWordCount();
-                    joiner.add(s);
+                    String line = wordInfo.getValue() + " " + wordInfo.getWordCount();
+                    joiner.add(line);
                 }
                 return joiner.toString();
             } catch (Exception e) {
