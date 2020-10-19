1a700490e5d83a6efadc65237f145d0ab0c65aed
MANALCH <manalch@oocl.com>
2020-10-19 15:41:26 +0800

    [Word-Frequency][joseph] refactor: change variable name from inputList to to wordInfoList

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -13,26 +13,26 @@ public class WordFrequencyGame {
                 //split the input string with 1 to n pieces of spaces
                 String[] words = sentence.split(WHITE_SPACES);
 
-                List<WordInfo> inputList = new ArrayList<>();
+                List<WordInfo> wordInfoList = new ArrayList<>();
                 for (String s : words) {
                     WordInfo input = new WordInfo(s, 1);
-                    inputList.add(input);
+                    wordInfoList.add(input);
                 }
 
                 //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map = getListMap(inputList);
+                Map<String, List<WordInfo>> map = getListMap(wordInfoList);
 
                 List<WordInfo> list = new ArrayList<>();
                 for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
                     WordInfo input = new WordInfo(entry.getKey(), entry.getValue().size());
                     list.add(input);
                 }
-                inputList = list;
+                wordInfoList = list;
 
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
-                for (WordInfo w : inputList) {
+                for (WordInfo w : wordInfoList) {
                     String s = w.getValue() + " " + w.getWordCount();
                     joiner.add(s);
                 }
